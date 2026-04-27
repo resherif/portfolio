1aff7f1f9b94373e01ce1a9d6b955c89ba80501e
reham sherif <rehamsherrif@gmail.com>
2026-04-27 13:55:55 +0300

    r

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
 import { ExternalLink } from 'lucide-react';
 import { projects,categories} from '../data';
-import Skills from './Skills';
-
 const Projects = () => {
     const [active, setActive] = useState("all");
     const visible = projects.filter(
@@ -10,11 +8,11 @@ const Projects = () => {
   )
   return (
     <section className="py-16 px-6 max-w-5xl mx-auto " id="Projects">
-        <div  className='flex justify-center items-center gap-6 ' >
+        <div  className='grid  justify-center items-center gap-6 sm:gap-1.5 sm:grid-cols-2 ' >
         {categories.map((cat) => (
           <button
             key={cat.key}
-            className={`p-4 bg-indigo-800/45 text-lg mb-10 rounded-2xl  ${active === cat.key ?"bg-blue-600 text-indigo-400" : "bg-gray-200 text-slate-900"}`}
+            className={`p-4 sm:p-1.5 bg-indigo-800/45 text-lg mb-10 rounded-xl  ${active === cat.key ?"bg-blue-600 text-indigo-400" : "bg-gray-200 text-slate-900"}`}
             onClick={() => setActive(cat.key)}
           >
             {cat.label}
@@ -25,7 +23,7 @@ const Projects = () => {
           {visible.map((project) => (
             <div key={project.title} className="bg-indigo-800/20 p-6 rounded-xl shadow-sm  hover:shadow-md transition-shadow">
               <div className="flex justify-between items-start mb-4">
-                <span className="text-xs font-bold text-blue-600 uppercase tracking-tighter text-indigo-700 px-2 py-1 rounded">
+                <span className="text-xs font-bold  uppercase tracking-tighter text-indigo-700 px-2 py-1 rounded">
                   {project.projCategory}
                 </span>
               <a href={project.github} target="_blank" rel="noopener noreferrer">   <ExternalLink size={24} className="text-indigo-800"/></a>
